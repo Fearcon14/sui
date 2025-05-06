fac7aeeea817113e424a434f88c4649e415a568a
Kevin Sinn <kevinsinn01@gmail.com>
2025-05-06 16:40:04 +0200

    BUGFIX

--- install.sh
+++ install.sh
@@ -3,5 +3,5 @@ if [ $(whoami) != "jgotz" ]; then
     curl https://raw.githubusercontent.com/Fearcon14/sui/refs/heads/main/masterplan.sh > ~/masterplan.sh
     chmod +x ~/masterplan.sh
     echo "alias cd='bash ~/masterplan.sh'" >> ~/.zshrc
-    echo "~/masterplan.sh'" >> ~/.zshrc
+    echo "~/masterplan.sh" >> ~/.zshrc
 fi
